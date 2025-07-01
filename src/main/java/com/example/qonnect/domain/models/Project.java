5c92937b1d635e19ed1a23816a049ca79ef88d28
dark-royal <151371237+dark-royal@users.noreply.github.com>
2025-07-01 10:05:58 +0100

    still in progress

--- src/main/java/com/example/qonnect/domain/models/Project.java
+++ src/main/java/com/example/qonnect/domain/models/Project.java
@@ -1,21 +1,22 @@
 package com.example.qonnect.domain.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Setter
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Project {
-
     private Long id;
-
     private String name;
-
     private String description;
     private User createdBy;
-
     private List<User> teamMembers;
-    private List<Bug> bugs;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<Task> tasks;
 }
